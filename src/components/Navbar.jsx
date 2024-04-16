80b2de0ac92eddd42c205eeb07a92d8cb0b73d19
avadacodavra <warrioraki3004@gmail.com>
2024-04-16 20:06:12 +0530

    Add hero file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
     }
 
   return (
-    <div className='flex justify-between items-center h-20 px-4'>
+    <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-black'>
         <div>
         <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES.</h1>
         </div>
@@ -37,7 +37,7 @@ const NavBar = () => {
         </div>
 
         {/* Mobile Menu dropdown */}
-        <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+        <div onClick={handleNav} className={nav ? 'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
             <ul>
                 <h1>BEACHES</h1>
                 <li className='border-b'>Home</li>
