cd2f666721880bd8b96c7dcfc44c32cb37d4e8f1
harshilmalhotra <malhotraharshil2@gmail.com>
2024-04-17 00:12:54 +0530

    Hero issue resolved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from './components/Navbar';
-
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/hero.jsx';
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   );
 }
