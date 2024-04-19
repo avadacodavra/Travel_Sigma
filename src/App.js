c2c62c5d5e8326c4ec34aadcaacd618d9b9b9fb8
avadacodavra <143059020+avadacodavra@users.noreply.github.com>
2024-04-19 15:37:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Hero from './components/hero.jsx';
 import Destinations from './components/Destinations.jsx';
 import Search from './components/Search.jsx';
 import Selects from './components/Selects.jsx';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Carousel from './components/Carousel.jsx';
+import Footer from './components/Footer.jsx';
 
 
 
