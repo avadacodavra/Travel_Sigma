14827d6f225db5065e09bceee4a3e1f97224b856
avadacodavra <warrioraki3004@gmail.com>
2024-04-19 11:18:13 +0530

    Add Search component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/hero.jsx';
 import Destinations from './components/Destinations.jsx';
+import Search from './components/Search.jsx';
+
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
       <Destinations></Destinations>
+      <Search></Search>
     </div>
   );
 }
